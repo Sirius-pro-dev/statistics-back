37b64d61273fe29402d9feb797600729639dc242
Danilov_G <gleb22d@mail.ru>
2023-10-04 14:43:32 +0200

    mongo

--- src/routes/student/index.ts
+++ src/routes/student/index.ts
@@ -1,19 +1,19 @@
-const Student = require('../../models/student');
-const studentByID = require('../../json/students/byID/success.json')
+import Student from '../../models/student';
 const errorByID = require('../../json/students/byID/error.json')
-const grades = require('../../json/students/grades/success.json')
-const gradesError = require('../../json/students/grades/error.json')
 
 async function routes(fastify) {
   fastify.get('/students/:id', async (request, reply) => {
     try {
-      const id  = request.params.id;
-      const student = await studentByID;
-      if (student.body.id != id) {
+      const {id} = request.params;
+      if (!/^[0-9a-fA-F]{24}$/.test(id)) {
+        return reply.status(400).send({ error: 'Некорректный формат идентификатора', success: false });
+      }
+      const student = await Student.findById(id);
+      if (!student) {
         reply.status(404).send(errorByID);
         return;
       }
-      reply.status(201).send(studentByID.body);
+      reply.status(200).send(student);
     } catch (error) {
       console.error(error);
       reply.status(500).send('Internal Server Error');
@@ -21,13 +21,16 @@ async function routes(fastify) {
   });
   fastify.get('/students/:id/grades', async (request, reply) => {
     try {
-      const id = request.params.id;
-      const student = await studentByID;
-      if (student.body.id != id) {
-        reply.status(404).send(gradesError);
+      const {id} = request.params;
+      if (!/^[0-9a-fA-F]{24}$/.test(id)) {
+        return reply.status(400).send({ error: 'Некорректный формат идентификатора', success: false });
+      }
+      const student = await Student.findById(id);
+      if (!student) {
+        reply.status(404).send(errorByID);
         return;
       }
-      reply.status(200).send(grades);
+      reply.status(200).send(student.grades);
     } catch (error) {
       console.error(error);
       reply.status(500).send('Internal Server Error');
@@ -35,15 +38,32 @@ async function routes(fastify) {
   });
   fastify.post('/students/:id/grades', async (request, reply) => {
     try {
-      const id = request.params.id;
-      const student = await studentByID;
-      if (student.body.id != id) {
-        reply.code(404).send(errorByID);
+      const {id} = request.params;
+      if (!/^[0-9a-fA-F]{24}$/.test(id)) {
+        return reply.status(400).send({ error: 'Некорректный формат идентификатора', success: false });
+      }
+      const student = await Student.findById(id);
+      if (!student) {
+        reply.status(404).send(errorByID);
         return;
       }
-      const grade = request.body.grade;
-      student.body.grades.push(grade);
-      reply.status(201).send(grades);
+      const { grade } = request.body;
+      if (!grade) {
+        return reply.status(400).send({ error: 'Некорректный формат тела запроса', success: false });
+      }
+      student.grades.push(grade);
+      await student.save();
+      reply.status(201).send({message: 'Оценка успешно добавлена', success: true});
+    } catch (error) {
+      console.error(error);
+      reply.code(500).send('Internal Server Error');
+    }
+  });
+  fastify.post('/students/', async (request, reply) => {
+    try {
+      const student = new Student(request.body);
+      await student.save();
+      reply.status(201).send({message: 'Студент создан', success: true});
     } catch (error) {
       console.error(error);
       reply.code(500).send('Internal Server Error');
