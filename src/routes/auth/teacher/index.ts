355316533a4ae2c8b06672d1718a969059a51ddb
Danilov_G <gleb22d@mail.ru>
2023-10-13 17:36:29 +0200

    fix: teacher reg

--- src/routes/auth/teacher/index.ts
+++ src/routes/auth/teacher/index.ts
@@ -11,22 +11,22 @@ const generateAccessToken = (id, role) => {
 }
 
 async function routes(fastify) {
-  fastify.post('/teachers/registration', async (request, reply) => {
-    try {
-      const {email, password, firstname, lastname, subject} = request.body;
-      const candidate = await Teacher.findOne({email});
-      if (candidate) {
-        return reply.status(400).send({message: "Пользователь с такой почтой уже существует", success: false});
-      }
-      const hashPassword = bcrypt.hashSync(password, 7);
-      const teacher = new Teacher({email, password: hashPassword, firstname, lastname, subject});
-      teacher.save();
-      reply.status(201).send({message: "Пользователь успешно зарегестрирован", success: true});
-    } catch (error) {
-      console.error(error);
-      reply.status(500).send('Internal Server Error');
-    }
-  });
+  // fastify.post('/teachers/registration', async (request, reply) => {
+  //   try {
+  //     const {email, password, firstname, lastname, subject} = request.body;
+  //     const candidate = await Teacher.findOne({email});
+  //     if (candidate) {
+  //       return reply.status(400).send({message: "Пользователь с такой почтой уже существует", success: false});
+  //     }
+  //     const hashPassword = bcrypt.hashSync(password, 7);
+  //     const teacher = new Teacher({email, password: hashPassword, firstname, lastname, subject});
+  //     teacher.save();
+  //     reply.status(201).send({message: "Пользователь успешно зарегестрирован", success: true});
+  //   } catch (error) {
+  //     console.error(error);
+  //     reply.status(500).send('Internal Server Error');
+  //   }
+  // });
   fastify.post('/teachers/login', async (request, reply) => {
     try {
       const {email, password} = request.body;
